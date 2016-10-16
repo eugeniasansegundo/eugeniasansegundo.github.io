ea8f8bc973f137a6d1913ffa186c410c138a9558
unknown <eugeniasansegundo@gmail.com>
2016-10-16 12:59:06 +0200

    dynamic text decoration in navbar

--- js/my.js
+++ js/my.js
@@ -14,9 +14,32 @@ $(document).ready(function(){
 
     });
 
+    $('.wsite-menu-item-wrap').click(function(){
+        $('.selected').removeClass("selected");
+        $(this).toggleClass("selected");
+
+
+    });
+
     $('.wsite-menu-item-wrap').mouseenter(function(){
-        $(this).fadeTo('fast',1);
-    })
-    ;
+        if ($(this).hasClass("selected")){
+            /*do nothing*/
+        }
+        else {
+            $(this).fadeTo('fast', 1);
+            $(this).css('text-decoration', 'underline overline');
+        }
+    }) ;
+
+    $('.wsite-menu-item-wrap').mouseleave(function(){
+        if ($(this).hasClass("selected")){
+            /*do nothing*/
+        }
+        else {
+            $(this).fadeTo('fast', 0.5);
+            $(this).css('text-decoration', 'none');
+        }
+    }) ;
+
 
 });
