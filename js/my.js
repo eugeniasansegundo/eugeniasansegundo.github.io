49093e7405cf802458a5b5c4a849ee4a2d7c2ce4
unknown <eugeniasansegundo@gmail.com>
2016-10-17 00:30:27 +0200

    Cloud word dummy attempt

--- js/my.js
+++ js/my.js
@@ -62,5 +62,21 @@ $(document).ready(function(){
         }
     }) ;
 
+    var words = [
+        {text: "Lorem", weight: 13},
+        {text: "Ipsum", weight: 10.5},
+        {text: "Dolor", weight: 9.4},
+        {text: "Sit", weight: 8},
+        {text: "Amet", weight: 6.2},
+        {text: "Consectetur", weight: 5},
+        {text: "Adipiscing", weight: 5},
+        /* ... */
+    ];
+
+    $('#keywords').jQCloud(words, {
+        width: $('#keywords').width(),
+        height: $('#keywords').height()
+    });
+
 
 });
