f93043b1e5c36c93b8c2fc4a0cccb37cc80156ce
eugeniasansegundo <eugeniasansegundo@gmail.com>
2017-06-05 10:38:26 +0200

    pre-final version

--- vas/js/vas.js
+++ vas/js/vas.js
@@ -21,6 +21,59 @@ $(document).ready(function(){
 
     });
 
+    $('.next_button').click(function(){
+
+
+        var r = confirm("Did you send the values for this speaker? Please do it before doing the next one");
+
+        if (r==true) {
+
+            var username = document.getElementById('username').value;
+
+            console.log(username);
+            document.getElementById('myForm').reset();
+
+            document.getElementById('username').setAttribute('value', username);
+
+
+            var audio = document.getElementById('player');
+            var source = document.getElementById('wavsource');
+
+
+            //console.log(source.getAttribute('src'));
+            //console.log(audio.src);
+
+            var currentfile = parseInt(source.getAttribute('src').substr(4, 6));
+
+            var newsource = 0;
+            if (currentfile == 24) {
+                newsource = 1;
+            }
+            else {
+                newsource = currentfile + 1;
+            }
+
+            var newfile = 'aud/' + newsource + '.wav'
+            //console.log('current: ' +currentfile);
+            //console.log(newfile);
+            source.setAttribute('src', newfile);
+            audio.load();
+            audio.play();
+
+
+            document.getElementById("playing").setAttribute('value', newsource + '.wav');
+
+        }
+
+        else {
+
+        }
+
+
+
+
+    });
+
 
 
 });
